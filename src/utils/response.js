c00ed863c71c6f84e63f465b17e94518245469d3
kennedfer <kenned14santos@gmail.com>
2023-11-22 08:45:38 -0300

    'response.js' util's documented

--- src/utils/response.js
+++ src/utils/response.js
@@ -5,6 +5,14 @@
  * @returns {Object} Um objeto de resposta contendo propriedades específicas do usuário seguindo modelo especificado.
  */
 export const createResponse = (user) => {
+    /**
+     * Objeto de resposta contendo as seguintes propriedades:
+     * @property {string} id - O ID do usuário, extraído da chave '_id'.
+     * @property {string} data_criacao - A data de criação do usuário, extraída da chave 'data_criacao'.
+     * @property {string} data_atualizacao - A última data de atualização do usuário, extraída da chave 'data_atualizacao'.
+     * @property {string} ultimo_login - A última data de login do usuário, extraída da chave 'ultimo_login'.
+     * @property {string} token - O token do usuário, extraído da chave 'token'.
+     */
     return {
         id: user['_id'],
         data_criacao: user['data_criacao'],

68797312d7a9d11c8561296434fd45b88e3c2623
kennedfer <kenned14santos@gmail.com>
2023-11-22 09:02:42 -0300

    'date.js' util's documented

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,13 @@
+/**
+ * Obtém a data e hora atuais.
+ *
+ * @returns {Date} O objeto Date representando a data e hora atuais.
+ */
 export const getCurrentDate = () => {
+    /**
+     * @class Date
+     * @constructs {number} milliseconds - O número de milissegundos desde 1 de janeiro de 1970 00:00:00 UTC.
+     * @returns {Date} O objeto Date representando a data e hora atuais.
+     */
     return new Date(Date.now());
 }

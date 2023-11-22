3755b16adc6dc12efc3450641fe8a351aa30bff8
kennedfer <kenned14santos@gmail.com>
2023-11-22 08:56:08 -0300

    'encrypt.js' util's documented

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,11 +1,40 @@
 import bcrypt from "bcrypt";
 
-const saltRounds = 10;
+/**
+ * Número de rounds para a geração de sal usado no algoritmo de hash.
+ * @constant {number}
+ */
+const SALT_ROUNDS = 10;
 
+/**
+ * Gera um hash bcrypt sincronizado para os dados fornecidos.
+ *
+ * @param {string} data - Os dados a serem hashificados.
+ * @returns {string} O hash bcrypt gerado.
+ */
 export const hashEncrypt = (data) => {
-    return bcrypt.hashSync(data, saltRounds);
+    /**
+     * @function bcrypt.hashSync
+     * @param {string} data - Os dados a serem 'hashificados'.
+     * @param {number} saltRounds - O número de rounds para a geração do sal.
+     * @returns {string} O hash bcrypt gerado.
+     */
+    return bcrypt.hashSync(data, SALT_ROUNDS);
 }
 
+/**
+ * Compara dados com um hash bcrypt para verificar correspondência.
+ *
+ * @param {string} data - Os dados a serem comparados.
+ * @param {string} encryptData - O hash bcrypt a ser comparado.
+ * @returns {boolean} true se os dados correspondem ao hash, false caso contrário.
+ */
 export const match = (data, encryptData) => {
+    /**
+     * @function bcrypt.compareSync
+     * @param {string} data - Os dados a serem comparados.
+     * @param {string} encryptData - O hash bcrypt a ser comparado.
+     * @returns {boolean} true se os dados correspondem ao hash, false caso contrário.
+     */
     return bcrypt.compareSync(data, encryptData);
 }

c03f3e111d5f2146768e37a178ed47b88748b9e8
kennedfer <kenned14santos@gmail.com>
2023-11-22 09:26:29 -0300

    "models.js" documented

--- src/models/models.js
+++ src/models/models.js
@@ -1,5 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
+/**
+ * Definição do esquema mongoose para usuários.
+ * @typedef {Object} UserSchema
+ * @property {string} nome - O nome do usuário.
+ * @property {string} email - O e-mail do usuário.
+ * @property {string} senha - A senha do usuário.
+ * @property {Date} data_criacao - A data de criação do usuário.
+ * @property {Date} data_atualizacao - A última data de atualização do usuário.
+ * @property {Date} ultimo_login - A última data de login do usuário.
+ * @property {Object[]} telefones - Um array de objetos representando os números de telefone do usuário.
+ */
 const userSchema = new Schema({
     nome: {
         type: String,
@@ -38,4 +49,8 @@ const userSchema = new Schema({
     }
 });
 
+/**
+ * Modelo mongoose para usuários.
+ * @constant {Model} Users
+ */
 export const Users = mongoose.model("Users", userSchema);
